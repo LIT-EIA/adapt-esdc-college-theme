97c55e1e1df223fc67d16e65e32c014be1de90cc
petchen123 <peterchen_karwing@outlook.com>
2024-03-13 15:14:02 -0400

    added logic to handle component width ratio

--- js/esdc.js
+++ js/esdc.js
@@ -7,6 +7,19 @@ define(function(require) {
 	// Block View
 	// ==========
 
+  Adapt.on('componentView:postRender', function (view) {
+    var parentBlock = this.blocks._byAdaptID[view.model.get('_parentId')][0];
+
+    if (view.model.get('_layout') === 'left') {
+      var widthPercentage = parentBlock.get('componentWidthRatio');
+      view.$el.css('width', `${widthPercentage}%`);
+    }
+    else if (view.model.get('_layout') === 'right') {
+      var widthPercentage = 100 - parseInt(parentBlock.get('componentWidthRatio'));
+      view.$el.css('width', `${widthPercentage}%`);
+    }
+  });
+
 	Adapt.on('blockView:postRender', function(view) {
 		var theme = view.model.get('_theme');
 		
