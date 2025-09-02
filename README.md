217fc96448f81396ca70ed7f8d4f3f828393d5a8
GGD4V <david.dion@me.com>
2025-09-02 07:44:17 -0400

    color rework & banner waves

--- README.md
+++ README.md
@@ -16,16 +16,16 @@
 | Color Name | Hex | Variable name |
 | :------------- | :------------- | :------------- |
 |Main blue color|![#26374A](https://placehold.co/15x15/26374A/26374A.png) `#26374A`| `@esdc-blue` |
-|Accent blue|![#1C578A](https://placehold.co/15x15/1C578A/1C578A.png) `#1C578A`| `@esdc-interactive-blue` |
-|Accent blue hover|![#1C578A](https://placehold.co/15x15/1C578A/1C578A.png) `#1C578A`| `@esdc-int-hover-color` |
-|Bright highlights pink|![#CD1C6A](https://placehold.co/15x15/CD1C6A/CD1C6A.png) `#CD1C6A`| `@esdc-pink` |
-|Lesser highlights purple|![#69459C](https://placehold.co/15x15/69459C/69459C.png) `#69459C`| `@esdc-purple` |
-|Background alternate|![#E6E6E6](https://placehold.co/15x15/E6E6E6/E6E6E6.png) `#E6E6E6`| `@esdc-lightgrey` |
-|White|![#FFFFFF](https://placehold.co/15x15/FFFFFF/FFFFFF.png) `#FFFFFF`| `@esdc-white` |
-|White Hover|![#DEDEDE](https://placehold.co/15x15/DEDEDE/DEDEDE.png) `#DEDEDE`| `@esdc-white-hover` |
-|Black|![#000000](https://placehold.co/15x15/000000/000000.png) `#000000`| `@esdc-black` |
-|Correct color|![#56cdb0](https://placehold.co/15x15/56cdb0/56cdb0.png) `#56cdb0`| `@esdc-correct` |
-|Incorrect color|![#CD1C6A](https://placehold.co/15x15/CD1C6A/CD1C6A.png) `#CD1C6A`| `@esdc-incorrect` |
+|Accent blue|![#1C578A](https://placehold.co/15x15/1C578A/1C578A.png) `#1C578A`| `@color-accent` |
+|Accent blue hover|![#1C578A](https://placehold.co/15x15/1C578A/1C578A.png) `#1C578A`| `@color-accent` |
+|Bright highlights pink|![#CD1C6A](https://placehold.co/15x15/CD1C6A/CD1C6A.png) `#CD1C6A`| `@color-accent` |
+|Lesser highlights purple|![#69459C](https://placehold.co/15x15/69459C/69459C.png) `#69459C`| `@color-accent` |
+|Background alternate|![#E6E6E6](https://placehold.co/15x15/E6E6E6/E6E6E6.png) `#E6E6E6`| `@color-secondary` |
+|White|![#FFFFFF](https://placehold.co/15x15/FFFFFF/FFFFFF.png) `#FFFFFF`| `@color-background` |
+|White Hover|![#DEDEDE](https://placehold.co/15x15/DEDEDE/DEDEDE.png) `#DEDEDE`| `@color-secondary` |
+|Black|![#000000](https://placehold.co/15x15/000000/000000.png) `#000000`| `@color-text` |
+|Correct color|![#56cdb0](https://placehold.co/15x15/56cdb0/56cdb0.png) `#56cdb0`| `@color-success` |
+|Incorrect color|![#CD1C6A](https://placehold.co/15x15/CD1C6A/CD1C6A.png) `#CD1C6A`| `@color-accent` |
 
 ## Settings overview
 
--- js/esdc.js
+++ js/esdc.js
@@ -1,11 +1,11 @@
-define(function(require) {
-	
-	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
-	var ThemeBlock = require('theme/adapt-contrib-vanilla/js/theme-block');
+define(function (require) {
 
-	// Block View
-	// ==========
+  var Adapt = require('coreJS/adapt');
+  var Backbone = require('backbone');
+  var ThemeBlock = require('theme/adapt-contrib-vanilla/js/theme-block');
+
+  // Block View
+  // ==========
 
   Adapt.on('componentView:postRender', function (view) {
     var parentBlock = this.blocks._byAdaptID[view.model.get('_parentId')][0];
@@ -27,16 +27,59 @@ define(function(require) {
     }
   });
 
-	Adapt.on('blockView:postRender', function(view) {
-		var theme = view.model.get('_theme');
-		
-		if (theme) {
-			new ThemeBlock({
-				model: new Backbone.Model({
-					_themeBlockConfig: theme
-				}),
-				el: view.$el
-			});
-		}
-	});
+  Adapt.on('blockView:postRender', function (view) {
+    var theme = view.model.get('_theme');
+
+    if (theme) {
+      new ThemeBlock({
+        model: new Backbone.Model({
+          _themeBlockConfig: theme
+        }),
+        el: view.$el
+      });
+    }
+    
+  });
+  Adapt.on('menuView:postRender', function (view) {
+    const $menuHeader = view.$el.find('.menu-header');
+    if (!$menuHeader.length) return;
+    if ($menuHeader.find('.custom-svg-wrapper').length) return;
+    const svg = `
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+  <defs>
+    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+  </defs>
+  <g class="parallax">
+    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+    <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+  </g>
+</svg>
+  `;
+    $menuHeader.append(svg);
+  });
+
+    Adapt.on('pageView:postRender', function (view) {
+    const $pageHeader = view.$el.find('.page-header');
+    if (!$pageHeader.length) return;
+    if ($pageHeader.find('.custom-svg-wrapper').length) return;
+    const svg = `
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+  <defs>
+    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+  </defs>
+  <g class="parallax">
+    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+    <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+  </g>
+</svg>
+  `;
+    $pageHeader.append(svg);
+  });
+
+
 });
+
